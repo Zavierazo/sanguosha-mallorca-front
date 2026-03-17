8cd2411d6ebf541d4155b119e582141dd28f05dc
Arcan <unarcangelsoy@yahoo.es>
2026-03-17 12:16:02 +0100

    Excluyo las carpetas de linting para que no haga lintado de archivos en assets.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,12 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ["dist/**", "build/**", "node_modules/**"] },
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: { react: { version: "detect" } }
+  },
 ];
